edf478c6ae057384feb3a970c53237c3f9a310aa
Danylo Chernov <97985893+Fyoqusi@users.noreply.github.com>
2025-11-10 10:41:24 +0100

    Fix everything

--- Google Home Page/script.js
+++ Google Home Page/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function toggleSignInPopUp() {
   const signInPopUp = document.getElementById("SignInPopUp");
   if (signInPopUp.style.display === "none") {
@@ -7,13 +6,3 @@ function toggleSignInPopUp() {
     signInPopUp.style.display = "none";
   }
 }
-=======
-function toggleSignInPopUp() {
-  const signInPopUp = document.getElementById("SignInPopUp");
-  if (signInPopUp.style.display === "none") {
-    signInPopUp.style.display = "block";
-  } else {
-    signInPopUp.style.display = "none";
-  }
-}
->>>>>>> c016f3030eabcd66ed1d53eb9404fac30e4bfbfb
