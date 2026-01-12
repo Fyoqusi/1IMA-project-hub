dba3fa31c9b2d7bc75732730de752fce7de12905
Danylo Chernov <97985893+Fyoqusi@users.noreply.github.com>
2026-01-12 08:35:14 +0100

    Adding For Loops task

---
+++ ForLoops/script.js
@@ -0,0 +1,48 @@
+// Første oppgave
+console.log("Første oppgave");
+for (let num = 0; num < 10; num++) {
+  console.log("Hello World!");
+}
+// Andre oppgave
+console.log("Andre oppgave");
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+// Tredje oppgave
+console.log("Tredje oppgave");
+for (let i = 5; i <= 8; i++) {
+  console.log(i);
+}
+// Fjerde oppgave
+console.log("Fjerde oppgave");
+for (let i = 7; i >= 3; i--) {
+  console.log(i);
+}
+// Femte oppgave
+console.log("Femte oppgave");
+for (let i = 2; i <= 10; i = i + 2) {
+  console.log(i);
+}
+// Sjette oppgave
+console.log("Sjette oppgave");
+for (let i = 9; i >= 1; i = i - 2) {
+  console.log(i);
+}
+// Sjuende oppgave
+console.log("Sjuende oppgave");
+let summen = 0;
+for (let num = 1; num <= 4; num++) {
+  summen += num;
+}
+console.log(summen);
+// Åttende oppgave
+console.log("Åttende oppgave");
+let hashtags = "";
+for (let hash = 1; hash <= 4; hash++) {
+  console.log("#".repeat(hash));
+}
+// Niende oppgave
+document.writeln("Niende oppgave");
+for (h = 6; h >= 1; h--) {
+  document.writeln(`<h${h}>Røyken vgs er best.</h${h}>`);
+}
